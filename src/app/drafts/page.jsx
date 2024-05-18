dc13a393f7b182d2848a6310d4f3b14f95c91971
mattjoke <matejhakos@gmail.com>
2024-05-18 22:16:47 +0200

    updae ymla

--- src/app/drafts/page.jsx
+++ src/app/drafts/page.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import Image from 'next/image'
 import Post from '../../components/Post'
+import React from 'react'
 import prisma from '../../lib/prisma'
 import styles from '../../styles/Drafts.module.css'
 
@@ -12,6 +13,13 @@ const Drafts = async () => {
     <>
       <div>
         <h1>Drafts</h1>
+        <Image
+          alt="A humble cottage in the woods."
+          src="https://hakospa200du2.blob.core.windows.net/static-files/peter-thomas-QsjKrm2eeM4-unsplash.jpg?sp=r&st=2024-05-31T22:27:36Z&se=2024-06-29T06:27:36Z&spr=https&sv=2022-11-02&sr=b&sig=E5G2ltWvghnE3CAaVYrOwQDf7mg3t814bR4ucgYSJb0%3D"
+          width={2400}
+          height={1400}
+          layout="responsive"
+        />
         <main>
           {drafts.map((post) => (
             <div key={post.id} className={styles.post}>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
-import prisma from "../lib/prisma"
+import Image from "next/image"
 import Post from "../components/Post"
+import prisma from "../lib/prisma"
 
 export default async function Home() {
   const feed = await prisma.post.findMany({
@@ -13,6 +14,13 @@ export default async function Home() {
           <Post post={post} />
         </div>
       ))}
+      <Image
+        alt="A starry night sky."
+        src="https://hakospa200du2.blob.core.windows.net/static-files/arnaud-girault-zWOgtUIX7OE-unsplash.jpg?sp=r&st=2024-05-31T22:23:35Z&se=2024-07-06T06:23:35Z&spr=https&sv=2022-11-02&sr=b&sig=ej2t%2BXRY%2FNGVEowEK3xfzKqrvLa2TbkQntT9zG5VS1Y%3D"
+        width={2400}
+        height={1400}
+        layout="responsive"
+      />
     </>
   )
 }
