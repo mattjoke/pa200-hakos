6fb78761af123adb5c12e5e59771047daeb70c49
mattjoke <matejhakos@gmail.com>
2024-06-01 00:47:28 +0200

    build issue

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,13 +9,6 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         <div style={{ padding: "0 2rem" }}>
-          <Image
-            alt="A humble cottage in the woods."
-            src="https://hakospa200du2.blob.core.windows.net/static-files/peter-thomas-QsjKrm2eeM4-unsplash.jpg?sp=r&st=2024-05-31T22:27:36Z&se=2024-06-29T06:27:36Z&spr=https&sv=2022-11-02&sr=b&sig=E5G2ltWvghnE3CAaVYrOwQDf7mg3t814bR4ucgYSJb0%3D"
-            width={2400}
-            height={1400}
-            layout="responsive"
-          />
           {children}
         </div>
       </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,7 +19,6 @@ export default async function Home() {
         src="https://hakospa200du2.blob.core.windows.net/static-files/arnaud-girault-zWOgtUIX7OE-unsplash.jpg?sp=r&st=2024-05-31T22:23:35Z&se=2024-07-06T06:23:35Z&spr=https&sv=2022-11-02&sr=b&sig=ej2t%2BXRY%2FNGVEowEK3xfzKqrvLa2TbkQntT9zG5VS1Y%3D"
         width={2400}
         height={1400}
-        layout="responsive"
       />
     </>
   )
